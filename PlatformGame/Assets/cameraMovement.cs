52bf9aee917be85eb849e02a2c589a94f649ea8e
Thamer <68731244+ThamerThanks@users.noreply.github.com>
2020-11-20 22:11:04 +0300

    Set camera to follow player x pos

---
+++ PlatformGame/Assets/cameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    public Transform cameraPos;
+    public Transform playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        cameraPos.position = new Vector2(playerPos.position.x, cameraPos.position.y);
+    }
+}
