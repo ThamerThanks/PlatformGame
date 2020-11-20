9c4d398cebb972ab70c3a5c119895c6f69af3341
Thamer <68731244+ThamerThanks@users.noreply.github.com>
2020-11-20 21:30:38 +0300

    Add walk animation sheet

--- PlatformGame/Assets/PlayerMovement.cs
+++ PlatformGame/Assets/PlayerMovement.cs
@@ -10,21 +10,32 @@ public class PlayerMovement : MonoBehaviour
 
     public float horizontalMove = 0f;
     bool jump = false;
+    bool crouch = false;
 
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
         }
     }
     void FixedUpdate()
     {
         // Move our character
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
 }
