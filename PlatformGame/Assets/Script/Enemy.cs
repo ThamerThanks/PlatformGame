e60c43a176bbbeb02041483b5614a37768a18d28
Thamer <68731244+ThamerThanks@users.noreply.github.com>
2020-11-22 12:51:13 +0300

    Add enemy hit and death animations

--- PlatformGame/Assets/Script/Enemy.cs
+++ PlatformGame/Assets/Script/Enemy.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Animator animator;
+
+    private Rigidbody2D rb;
+
     public int maxHealth = 100;
     int currentHealth;
+    private bool isFell = false;
 
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
+
+        animator.SetTrigger("Hurt");
 
         if(currentHealth <= 0)
         {
@@ -23,9 +31,32 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        if (collisionInfo.collider.name == "Tilemap_fg")
+        {
+            Debug.Log(collisionInfo.collider.name);
+            rb.gravityScale = 0;
+            GetComponent<Collider2D>().enabled = false;
+            rb.velocity = Vector2.zero;
+            isFell = true;
+        }
+    }
+
     void Die()
     {
         Debug.Log("Enemy died");
+
+        animator.SetBool("IsDead", true);
+
+        rb.gravityScale = 1;
+        if (isFell)
+        {
+            this.enabled = false;
+        }
+
     }
+
+
 
 }
